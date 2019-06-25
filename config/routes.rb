c03248a1f325d28e3453377d5e2dea06d95f606d
github username <github email address>
2019-06-25 13:04:06 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # NEW
+  get '/posts/new', to: "posts#new", as: "new_post"
+  # INDEX
+  get '/posts', to: "posts#index"
+  #CREATE
+  post '/posts', to: "posts#create"
+  #SHOW
+  get '/posts/:id', to: "posts#show"
 end
